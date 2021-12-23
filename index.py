af6689e25be18775ef1de73edfa5d010c47b5f34
Alireza Khodabakhsh <alireza.khodabakhsh1@yahoo.com>
2021-12-23 12:12:17 +0330

    torch version added

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import torch
+print(torch.__version__)
 dtype = torch.float
 device  = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 A=torch.tensor([1,2,3], dtype = dtype, device=device)
